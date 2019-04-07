3993b979f0b42878eae0b37ed3e92536875c184a
Eguzkiman <eguzki@kioru.com>
2019-04-07 10:24:06 -0500

    login button now lives in welcome view

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,36 +2,21 @@ import React from 'react';
 
 import Avatar from './avatar';
 
-const SIGN_IN_URL = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&scope=repo user`;
-
 export default function Navbar (props) {
-
-	let actionSection;
-	if (props.currentUser) {
-		actionSection = (
-			<div className="v-center">
-				<div className="username">{props.currentUser.name}</div>
-				<Avatar src={props.currentUser.avatarUrl}/>
-			</div>
-		);
-	} else {
-		actionSection = (
-			<div className="v-center">
-				<button onClick={() => window.location.href = SIGN_IN_URL }>
-					Sign in with GitHub
-				</button>
-			</div>
-		);
-	}
 
 	return (
 		<div className="navbar flex-grid">
 			<div className="col">
 				<h1>GitGantt</h1>
 			</div>
-			<div className="flex-grid ">
-				{actionSection}
-			</div>
+			{props.currentUser && (
+				<div className="flex-grid ">
+					<div className="v-center">
+						<div className="username">{props.currentUser.name}</div>
+						<Avatar src={props.currentUser.avatarUrl}/>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from 'components/navbar';
 
+const SIGN_IN_URL = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&scope=repo user`;
+
 export default function Welcome () {
 	return (
 		<div>
 			<Navbar/>
-			<p>Sign in to continue</p>
+			<button onClick={() => window.location.href = SIGN_IN_URL }>
+				Sign in with GitHub
+			</button>
 		</div>
 	);
 }
